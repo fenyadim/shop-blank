b24cbe9e0d77291e6775bc9e90fda5261f9a93f0
fenyadim <dimos9813@gmail.com>
2025-08-05 09:24:00 +0800

    Init

--- src/utils/jwtDecrypt.ts
+++ src/utils/jwtDecrypt.ts
@@ -2,7 +2,7 @@ import 'server-only'
 
 import { jwtVerify } from 'jose'
 
-const secretKey = process.env.JWT_SECRET
+const secretKey = .JWT_SECRET
 const encodedKey = new TextEncoder().encode(secretKey)
 
 export async function decrypt(session: string | undefined = '') {
