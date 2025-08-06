d370069847534daea1e23c72370398c3f45fc2fb
fenyadim <dimos9813@gmail.com>
2025-08-06 18:06:10 +0800

    feat: Стилизация header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,8 @@ import { Button } from './ui/button'
 
 export const Header = () => {
 	return (
-		<header className='flex justify-center'>
-			<nav className='flex gap-3'>
+		<header className='flex justify-center my-2'>
+			<nav className='relative flex gap-3 p-2 rounded-full overflow-hidden before:bg-foreground/30 before:backdrop-blur-md before:absolute before:inset-0 before:-z-10 *:rounded-full'>
 				<Button asChild>
 					<Link href='/'>Home</Link>
 				</Button>
